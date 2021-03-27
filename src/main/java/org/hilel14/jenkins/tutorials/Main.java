80f0a85a99aa0ff07eca795ff3ff6cd2f0d52bf8
Hilel New <hilel14@gmail.com>
2021-03-27 13:33:18 +0300

    add simple java class

---
+++ src/main/java/org/hilel14/jenkins/tutorials/Main.java
@@ -0,0 +1,12 @@
+package org.hilel14.jenkins.tutorials;
+
+/**
+ *
+ * @author hilel
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Jenkins pipeline test");
+    }
+}
